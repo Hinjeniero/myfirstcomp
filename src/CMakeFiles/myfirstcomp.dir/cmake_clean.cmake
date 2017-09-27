78aa693b950ac1c99d0582404237876949e817e1
sergieteja <sergiobs93@gmail.com>
2017-09-27 18:59:11 +0200

    Added example's source files

---
+++ src/CMakeFiles/myfirstcomp.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "myfirstcomp_automoc.cpp"
+  "CommonBehavior.cpp"
+  "CommonBehavior.h"
+  "DifferentialRobot.cpp"
+  "DifferentialRobot.h"
+  "Laser.cpp"
+  "Laser.h"
+  "ui_mainUI.h"
+  "CMakeFiles/myfirstcomp.dir/specificworker.cpp.o"
+  "CMakeFiles/myfirstcomp.dir/specificmonitor.cpp.o"
+  "CMakeFiles/myfirstcomp.dir/home/salabeta/robocomp/classes/rapplication/rapplication.cpp.o"
+  "CMakeFiles/myfirstcomp.dir/home/salabeta/robocomp/classes/qlog/qlog.cpp.o"
+  "CMakeFiles/myfirstcomp.dir/main.cpp.o"
+  "CMakeFiles/myfirstcomp.dir/genericmonitor.cpp.o"
+  "CMakeFiles/myfirstcomp.dir/commonbehaviorI.cpp.o"
+  "CMakeFiles/myfirstcomp.dir/genericworker.cpp.o"
+  "CMakeFiles/myfirstcomp.dir/CommonBehavior.cpp.o"
+  "CMakeFiles/myfirstcomp.dir/DifferentialRobot.cpp.o"
+  "CMakeFiles/myfirstcomp.dir/Laser.cpp.o"
+  "CMakeFiles/myfirstcomp.dir/myfirstcomp_automoc.cpp.o"
+  "../bin/myfirstcomp.pdb"
+  "../bin/myfirstcomp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myfirstcomp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
